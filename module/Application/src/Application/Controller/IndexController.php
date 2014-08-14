596cbf8a17485a2fde9b04127d9a5d20c5112fdf
Hans Egg <hansegg@mac.com>
2014-08-14 13:16:11 +0200

    HubSpot Analytics Code added

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -12,10 +12,50 @@ namespace Application\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
-class IndexController extends AbstractActionController
-{
-    public function indexAction()
-    {
+class IndexController extends AbstractActionController {
+
+    public function indexAction() {
+        if (isset($_COOKIE['contextery-autoplay'])) {
+            $autoplay = 'off';
+        } else {
+            // turn autoplay off for 1 day
+            setcookie('contextery-autoplay', 'off', time() + 60 * 60 * 24 * 1);
+            $autoplay = 'on';
+        }
+        return new ViewModel(array('autoplay' => $autoplay));
+    }
+
+    public function howItWorksAction() {
+        return new ViewModel;
+    }
+
+    public function referencesAction() {
+        return new ViewModel;
+    }
+
+    public function aboutAction() {
         return new ViewModel();
     }
+
+    public function artistsAction() {
+        return new ViewModel;
+    }
+
+    public function tipsAndTricksAction() {
+        return new ViewModel;
+    }
+
+    public function implementationAreasAction() {
+        return new ViewModel;
+    }
+
+    public function videoStylesAction() {
+        $selected_style = $this->params('param1', 'simplify');
+        return new ViewModel(array('selected_style' => $selected_style));
+    }
+
+    public function yourBenefitsAction() {
+        return new ViewModel(array('page'=>'yourBenefits'));
+    }
+
 }
